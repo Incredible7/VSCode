338f7259c73c4e938dcf98b45004a1ec2254c0b6
Laptop <3143319739@qq.com>
2022-04-16 11:50:57 +0800

    1

--- VS Code C++/Test/test.cpp
+++ VS Code C++/Test/test.cpp
@@ -1,22 +1,9 @@
 #include "Algorithm_in_Leetcode.h"
-using namespace std;
 
-struct Param{
-    string name;
-    int age;
-};
-
-void* param_test(void* p){
-    Param* param = (Param*)p;
-    cout<<"hello "<<param->name<<endl;
-    cout<<"age = "<<param->age<<endl;
-}
 
 int main() {
-    pthread_t tid;
-    Param*ptr = new Param();
-    ptr->name = "pthread";
-    ptr->age = 20;
-    pthread_create(&tid, NULL, param_test, ptr);
-    pthread_join(tid, NULL);
+    std::vector<bool> features(const widget& w);
+
+    bool high = features(w)[5];
+    
 }
